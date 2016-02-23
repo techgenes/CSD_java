071075e5dbf9e234388178022fd743462c7c3ad8
techgenes <achal.14bcs2026@abes.ac.in>
2016-02-24 01:05:56 +0530

    first class

---
+++ CsdClass1/src/Bulb.java
@@ -0,0 +1,17 @@
+/**
+ * Created by achal on 2/23/16.
+ */
+public class Bulb {
+    boolean isOnState;
+    String bulbColor;
+    int price;
+    int power;
+
+
+    public void onBulb(){
+        System.out.println("Bulb is on");
+    }
+    public void tellColor(){
+        System.out.print("My color is yellow");
+    }
+}
---
+++ CsdClass1/src/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass{
+
+    public static void main(String[] args){
+        Bulb bulb1=new Bulb();
+        bulb1.onBulb();
+        bulb1.tellColor();
+    }
+}
